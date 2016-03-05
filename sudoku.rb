b72ce1f4ed608a88b7502ba02bc8576370016bea
John Maguire <jmaguire5588@gmail.com>
2016-03-05 17:23:31 -0500

    Worked on box check, still needs more work, need to create hash to save the row/column index of the cells we are taking

--- sudoku.rb
+++ sudoku.rb
@@ -169,17 +169,17 @@ def unique_possibilities_check(board)
     num_times = Hash.new
 
     arrays = row.select{|cell| cell.is_a?(Array) && cell.length > 0}.flatten!
-    if arrays != nil
-      arrays.each do |number|
-        if num_times.keys.include?(number)
-          num_times[number] += 1
-        else
-          num_times[number] = 1
-        end
-      end
+    	if arrays != nil
+      	arrays.each do |number|
+        	if num_times.keys.include?(number)
+          	num_times[number] += 1
+        	else
+          	num_times[number] = 1
+        	end
+      	end
       uniques[index] = num_times.select {|key,value| value == 1}.keys[0]
-    end
-  end
+     	end
+  	end
 
   board.each_with_index do |row, index|
     row.map! do |cell|
@@ -190,11 +190,10 @@ def unique_possibilities_check(board)
       end
     end
   end
-    board.transpose.each_with_index do |row, index|
-    arrays = []
+  board.transpose.each_with_index do |row, index|
+   	arrays = []
     num_times = Hash.new
-
-    arrays = row.select{|cell| cell.is_a?(Array) && cell.length > 0}.flatten!
+		arrays = row.select{|cell| cell.is_a?(Array) && cell.length > 0}.flatten!
     if arrays != nil
       arrays.each do |number|
         if num_times.keys.include?(number)
@@ -216,11 +215,73 @@ def unique_possibilities_check(board)
       end
     end
   end
-# binding.pry
-board
+
+	box_unique(board)
+ binding.pry
+	board
 
 end
 
+def box_unique(board)
+  uniques = {
+    0 => nil,
+    1 => nil,
+    2 => nil,
+    3 => nil,
+    4 => nil,
+    5 => nil,
+    6 => nil,
+    7 => nil,
+    8 => nil
+  }
+
+	row_column_coordinates = {
+    "top_left" => [[0,1,2],[0,1,2]],
+    "top_middle" => [[0,1,2],[3,4,5]],
+    "top_right" => [[0,1,2],[6,7,8]],
+    "middle_left" => [[3,4,5],[0,1,2]],
+    "middle" => [[3,4,5],[3,4,5]],
+    "middle_right" => [[3,4,5],[6,7,8]],
+    "bottom_left" => [[6,7,8],[0,1,2]],
+    "bottom_middle" => [[6,7,8],[3,4,5]],
+    "bottom_right" => [[6,7,8],[6,7,8]],
+  }
+
+	row_column_coordinates.each do |name, coordinates|
+	  coordinates[0].each do |row_num|
+	      coordinates[1].each do |col_num|
+	      	arrays = []
+	      	num_times = Hash.new
+	        if board[row_num][col_num].is_a?(Array) && board[row_num][col_num].length > 0
+	         arrays.push(board[row_num][col_num]).flatten!
+	       end
+	        if arrays != nil
+	     			arrays.each do |number|
+	       			if num_times.keys.include?(number)
+	         			num_times[number] += 1
+        			else
+	       				num_times[number] = 1
+	     				end
+	   				end
+	   			uniques[[row_num][col_num]] = num_times.select {|key,value| value == 1}.keys[0]
+	        end
+	     end
+   	end
+  end
+	 row_column_coordinates.each do |name, coordinates|
+	   coordinates[0].each do |row_num|
+  	   coordinates[1].each do |col_num|
+	        if board[row_num][col_num].is_a?(Array) && board[row_num][col_num].include?(uniques[[row_num][col_num]])
+	        	board[row_num][col_num] = uniques[[row_num][col_num]]
+	      	else
+	        	board[row_num][col_num]
+	      	end
+	    	end
+	  	end
+
+	 end
+	 board
+end
 
 #box unique check
 # hash coordinates
